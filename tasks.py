2df25ae5f4513475933fca4a0f59c8f59bba73a1
jonthus <joona.erkkila1@gmail.com>
2021-11-23 20:32:12 +0200

    Added tasks

---
+++ tasks.py
@@ -0,0 +1,21 @@
+from invoke import task
+
+@task
+def foo(ctx):
+    print("bar")
+
+@task
+def start(ctx):
+    ctx.run("python3 src/sequence.py")
+
+@task
+def test(ctx):
+    ctx.run("pytest src")
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src")
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html")
