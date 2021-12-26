ea5ee2d180b5c4af49d143d52539fba436381416
jonthus <joona.erkkila1@gmail.com>
2021-12-26 08:52:50 +0200

    Loppupalautus

---
+++ src/tests/validation_service_test.py
@@ -0,0 +1,23 @@
+
+import unittest
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from services import validation_service
+
+CORRECT_FILENAME = os.path.join(os.path.dirname(__file__), 'testfiles/histodata.txt')
+WRONG_FILENAME = os.path.join(os.path.dirname(__file__), 'testfiles/histogram.png')
+
+class TestValidationService(unittest.TestCase):
+
+    def test_file_type(self):
+        val = validation_service.file_type(CORRECT_FILENAME)
+        self.assertEqual(val, True)
+
+    def test_file_type_fails(self):
+        val = validation_service.file_type(WRONG_FILENAME)
+        self.assertEqual(val, False)
+
+    def test_is_fasta(self):
+        val = validation_service.is_fasta(CORRECT_FILENAME)
+        self.assertEqual(val, True)
